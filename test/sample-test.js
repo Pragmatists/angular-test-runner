f95e80c62ebc538d5b2fbd8371c1461e8657dfd6
Michael Bareja <michal.bareja@pragmatists.pl>
2016-11-16 14:05:32 +0100

    verify that keydown works

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,22 +3,30 @@ describe('sample test', function(){
   angular.module('greeting-app', [])
     .directive('greeting', function(){
       return {
-        template: ['<div>',
+        template: ['<form ng-submit="vm.sayHello()">',
                     '<input class="name" type=text ng-model="name">',
-                    '<button ng-click="vm.sayHello()">Say Hello</button>',
-                    '<span class="greeting">{{message}}',
-                   '</div>'].join(),
+                    '<button type="submit">Say Hello</button>',
+                    '<span class="greeting">{{message}}</span>',
+                   '</form>'].join(),
         scope: {
           name: '='
         },
         controllerAs: 'vm',
         controller: function($http, $scope){
           this.sayHello = function(){
+
             $http.post('/greeting', { name: $scope.name })
               .success(function(json){
                 $scope.message = json.greeting;
               });
           }
+        },
+        link: function(scope, element){
+          element.find('input').on('keydown', function(ev){
+            if(ev.which === 13){
+              scope.vm.sayHello();
+            }
+          });
         }
       };
     });
@@ -27,6 +35,7 @@ describe('sample test', function(){
   var type = testRunner.actions.type;
   var click = testRunner.actions.click;
   var expect = testRunner.actions.expectElement;
+  var keydown = testRunner.actions.keydown;
 
   beforeEach(function(){
 
@@ -40,9 +49,8 @@ describe('sample test', function(){
 
   beforeEach(function(){
     server.post('/greeting', function(req){
-      
+
       var body = req.body();
-      
       req.sendJson({
         greeting: 'Hello ' + body.name + '!'
       });
@@ -78,5 +86,23 @@ describe('sample test', function(){
     );
     
   });
+
+  it('greets person on enter', function(){
+    // given:
+    var html = app.runHtml('<greeting name="defaultName"/>', {defaultName: 'John'});
+
+    // when:
+    html.perform(
+        type('Jane').in('input.name'),
+        keydown(13).in('input.name')
+    );
+
+    // then:
+    html.verify(
+        expect('.greeting').toContainText('Hello Jane!')
+    );
+
+  });
+
 
 });
